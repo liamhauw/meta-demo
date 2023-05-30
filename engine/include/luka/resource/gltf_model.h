6d1f3f476ca77396b56007222f39bc9a2895ff7b
Liam Hauw <liamhauw@gmail.com>
2023-05-30 11:10:41 +0800

    commit

---
+++ engine/include/luka/resource/gltf_model.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace luka {
+class GltfModel {
+
+};
+}
---
+++ engine/src/resource/gltf_model.cc
@@ -0,0 +1,6 @@
+#include "luka/resource/gltf_model.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+#include "tiny_gltf.h"
