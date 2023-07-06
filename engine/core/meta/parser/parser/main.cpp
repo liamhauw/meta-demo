ab57c8cb2e435b585e43a238d960ed04406cad93
Liam Hauw <liamhauw@gmail.com>
2023-07-06 13:34:51 +0800

    commit

--- engine/core/meta/parser/parser/main.cpp
+++ engine/core/meta/parser/parser/main.cpp
@@ -1,42 +1,31 @@
 #include "common/precompiled.h"
 #include "parser/parser.h"
 
-void parse(std::string project_input_file_name, std::string source_include_file_name,
-           const std::string& include_path, std::string sys_include, const std::string& module_name,
-           const std::string& show_errors) {
-  std::cout << std::endl;
-  std::cout << "Parsing meta data for target \"" << module_name << "\"" << std::endl;
-  std::fstream input_file;
-
-  bool is_show_errors = "0" != show_errors;
-
-  MetaParser parser(project_input_file_name, source_include_file_name, include_path, sys_include,
-                    module_name, is_show_errors);
-
-  std::cout << "Parsing in " << include_path << std::endl;
-  int result = parser.parse();
-  if (0 != result) {
-    return;
+void Parse(int argc, char* argv[]) {
+  if (argc != 7) {
+    throw std::runtime_error{"argument count error"};
   }
 
-  parser.generateFiles();
+  std::string json_header_file{argv[1]};
+  std::string header_file{argv[2]};
+  std::string generated_path{argv[3]};
+  std::string system_include_path{argv[4]};
+  std::string module_name{argv[5]};
+  std::string show_error{argv[6]};
+
+  bool is_show_error = show_error != "0";
+
+  MetaParser meta_parser{json_header_file,    header_file, generated_path,
+                         system_include_path, module_name, is_show_error};
+  meta_parser.parse();
+  meta_parser.generateFiles();
 }
 
-int main(int, char* argv[]) {
-  if (argv[1] != nullptr && argv[2] != nullptr && argv[3] != nullptr && argv[4] != nullptr &&
-      argv[5] != nullptr && argv[6] != nullptr) {
-    MetaParser::prepare();
-
-    parse(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
-
-  } else {
-    std::cerr
-        << "Arguments parse error!" << std::endl
-        << "Please call the tool like this:" << std::endl
-        << "meta_parser  project_file_name  include_file_name_to_generate  project_base_directory "
-           "sys_include_directory module_name showErrors(0 or 1)"
-        << std::endl
-        << std::endl;
+int main(int argc, char* argv[]) {
+  try {
+    Parse(argc, argv);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
     return -1;
   }
 
