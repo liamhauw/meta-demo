10115532607232ebf25aed0c3f947d4ba90c0587
Liam Hauw <liamhauw@gmail.com>
2023-08-08 23:07:05 +0800

    commit

--- engine/resource/res_type/common/world.h
+++ engine/resource/res_type/common/world.h
@@ -18,7 +18,7 @@ CLASS(WorldRes, Fields) {
   std::string m_name;
 
   // all level urls for this world
-  // std::vector<std::string> m_level_urls;
+   std::vector<std::string> m_level_urls;
 
   // the default level for this world, which should be first loading level
   std::string m_default_level_url;
