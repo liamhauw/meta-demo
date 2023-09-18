00912596dba52f83f4311748d6ed9eea839acc9e
Liam Hauw <88476188+liamhauw@users.noreply.github.com>
2023-09-18 14:08:11 +0800

    commit

--- engine/core/context/context.h
+++ engine/core/context/context.h
@@ -9,5 +9,7 @@
 #pragma once
 
 namespace luka {
+
 extern bool g_is_editor_mode;
+
 }
--- engine/core/json/json.h
+++ engine/core/json/json.h
@@ -1,3 +1,17 @@
+/*
+  SPDX license identifier: MIT
+
+  Copyright (C) 2023-present Liam Hauw.
+
+  Json header file.
+*/
+
 #pragma once
+
 #include <json11.hpp>
+
+namespace luka {
+
 using Json = json11::Json;
+
+}
